9852563f9335740ae4aeb0e61564acb925888ca2
abbyzimmardi <68709094+abbyzimmardi@users.noreply.github.com>
2022-04-02 16:16:37 -0400

    show/hide button

--- lab-9/javascript.js
+++ lab-9/javascript.js
@@ -3,3 +3,24 @@
 function newHeadline() {
     document.getElementById("headline").innerHTML = headlineTwo;
   }
+
+  //Define variables
+  var headlineArray = ["Next headline", "Another Headline" ,"Yet Another" , "Final Headline"];
+  var headlineIndex = 0;
+  var headlineLength = headlineArray.length;
+  var myLoop = setInterval(changeHeadline,1000);
+
+  //Function to loop through the headline array
+  function changeHeadline () {
+    var headlineElement = document.getElementById("headline");
+     var newHeadline = headlineArray[headlineIndex];
+      //alert(newHeadline);
+    headlineElement.innerHTML = newHeadline;
+      headlineIndex++;
+      	if ( headlineIndex >= headlineLength ) {
+  	clearInterval(myLoop);
+  	}
+  }
+
+  //run the function
+  window.onload = changeHeadline(myLoop);

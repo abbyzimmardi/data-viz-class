e2ec9bd35de944d9383827161397105f7f421808
abbyzimmardi <68709094+abbyzimmardi@users.noreply.github.com>
2022-04-12 17:38:44 -0400

    styles added

--- lab-10/javascript.js
+++ lab-10/javascript.js
@@ -1,22 +1,22 @@
 var ctx = document.getElementById('myChart').getContext('2d');
 
 var myChart = new Chart(ctx, {
-  type: 'bar',
+  type: 'line',
   data: {
-    labels: ['2015', '2016', '2017', '2018', '2019', '2020'],
+    labels: ['1800', '1825', '1850', '1875', '1900', '1925', '1950', '1975', '2000'],
     datasets: [{
-      label: '# of Apples',
-      data: [12, 19, 3, 5, 2, 3],
-      backgroundColor: [
-        '#ff0800'
-      ]
+      label: 'China',
+      data: [32, 32, 32, 31.96, 31.75, 31.55, 40.73, 63.26, 70.03],
+      fill: false,
+      backgroundColor: '#00b159',
+      borderColor: '#00b159'
     },
       {
-        label: '# of Oranges',
-        data: [22, 10, 23, 6, 22, 13],
-        backgroundColor: [
-          '#ffa500'
-        ]
+        label: 'India',
+        data: [22.44, 25.44, 25.44, 25.11, 18.37, 26.69, 34.92, 51.51, 62.34],
+        fill: false,
+        borderColor: '#00aebd',
+        backgroundColor: '#00aebd'
       }
   ]
   },
@@ -25,14 +25,14 @@ var myChart = new Chart(ctx, {
     plugins: {
       title: {
         display: true,
-        text: 'Apples and Oranges by year',
+        text: 'Life expectancy in the five most populous countries in the world',
         font: {
           size: 18
         }
       },
       subtitle: {
         display: true,
-        text: 'For all but one year, oranges outnumbered apples'
+        text: 'Life expectancy has risen steadily in the last 100 years'
       }
     },
     scales: {
